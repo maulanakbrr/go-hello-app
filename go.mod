42b440d82ef9f9703cbcf13eeb7c30a0e16cba6a
Akbar Maulana <akbar.maulana@inticorp.tech>
2024-11-05 16:40:24 +0700

    initiated first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/maulanakbrr/go-hello-app
+
+go 1.23.2
+
+require github.com/maulanakbrr/go-hello-mod v0.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_hello_mod "github.com/maulanakbrr/go-hello-mod"
+)
+
+func main() {
+	fmt.Println(go_hello_mod.SayHello())
+}
